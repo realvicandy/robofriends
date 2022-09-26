d89461ebbaa482e10504cad68f62a84c12eaf019
Your Name <you@example.com>
2022-09-26 15:03:32 -0700

    adding ErrorBoundry component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import CardList from '../components/CardList';
-import SearchBox from '../components/SearchBox.js'
+import SearchBox from '../components/SearchBox.js';
 import './App.css';
-import Scroll from '../components/Scroll.js'
+import Scroll from '../components/Scroll.js';
+import ErrorBoundry from '../components/ErrorBoundry';
 
 class App extends Component {
 	constructor() {
-		super()
+		super() //gives access to this.props from the Constructor's parent
 		this.state = {
 			robots: [],
 			searchfield: ''
@@ -35,7 +36,9 @@ class App extends Component {
 				<h1 className='f1'>RoboFriends</h1>
 				<SearchBox searchChange={ this.onSearchChange } />
 				<Scroll>
-					<CardList robots={ filteredRobots } />
+					<ErrorBoundry>
+						<CardList robots={ filteredRobots } />
+					</ErrorBoundry>
 				</Scroll>
 			</div>
 		);
