a2a90df79ebfdd4b8de6b6c4ddc19ffc19c2b325
Your Name <you@example.com>
2022-09-26 08:21:43 -0700

    Initial file dump

---
+++ src/components/Scroll.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Scroll = (props) => {
+	return (
+		<div style={{overflowY: 'scroll', border: 'none', height: '800px'}}>
+			{ props.children }
+		</div>
+	);
+}
+
+export default Scroll;
